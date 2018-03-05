bfe033296da499ef01fb9e5d6d4cf9b5604ac23b
Oscar Gonzalez <oscgonfer@gmail.com>
2018-03-06 00:06:04 +0100

    Electronics design assignment

---
+++ 06.WeekAssignment/Blink Code/Platformio/src/blink.cpp
@@ -0,0 +1,18 @@
+#include <Arduino.h>
+
+#define LED 7
+#define BUTTON 2
+
+
+void setup() {
+  pinMode(LED, OUTPUT);
+  pinMode(BUTTON, INPUT);
+}
+
+void loop() {
+  if (digitalRead(BUTTON) == HIGH) {
+      digitalWrite(LED, HIGH);   // turn the LED on (HIGH is the voltage level)
+  } else {
+      digitalWrite(LED, LOW);   // turn the LED on (HIGH is the voltage level)
+  }       
+}
